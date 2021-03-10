cd5923ace55f880aa6dbd4520928e511a884aa5d
robozatiTemp <70652269+robozatiTemp@users.noreply.github.com>
2021-03-10 15:58:58 +0000

    Create launch.sh

---
+++ .config/polybar/launch.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+
+# Terminate already running bar instances
+killall -q polybar
+
+# If all your bars have ipc enabled, you can also use 
+# polybar-msg cmd quit
+
+#Wait until the processes have been shut down
+
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+#Launch polybar
+polybar default &
